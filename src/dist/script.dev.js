b616a07ae84d14853686dc351f10d649f6ea64eb
michal <michalwagh@gmail.com>
2022-08-18 15:57:19 +0100

    DOM elements connected

--- src/dist/script.dev.js
+++ src/dist/script.dev.js
@@ -1,28 +1,79 @@
 "use strict";
 
-var numbers = document.querySelectorAll('.calculator__number');
-var operators = document.querySelectorAll('.calculator__operator');
-var ac = document.querySelector('.calculator__AC');
-var output = document.querySelector('.calculator__output');
-var previousResult = document.querySelector('.calculator__currentperation');
-var currentResult = document.querySelector('.calculator__previousoperation');
-var currrentReading = '';
-var previousReading = '';
-
-var updateResult = function updateResult() {
-  currentResult.innerText = currrentReading;
-  previousResult.innerText = previousReading;
-};
-
-var listingNumbers = function listingNumbers(number) {
-  currrentReading = currrentResult.toString() + number.toString();
-};
-
-console.log(numbers.innerText);
-numbers.forEach(function (number) {
-  number.addEventListener('click', function () {
-    console.log(number.innerText);
-    listingNumbers(number.innerText);
-    updateResult();
-  });
+var currentNumber = document.querySelector('.currentNumber');
+var calculatorNumbers = document.querySelectorAll('.calculator__number');
+var calculatorOperators = document.querySelectorAll('.calculator__operator');
+var calculatorOutput = document.querySelector('.calculator__output');
+var calculatorAc = document.querySelector('.calculator__AC');
+var result = '';
+var previousNum = '';
+var sign = '';
+calculatorNumbers.forEach(function (button) {
+  return button.addEventListener('click', listNumbers);
 });
+calculatorOperators.forEach(function (button) {
+  return button.addEventListener('click', operatorFunction);
+});
+calculatorOutput.addEventListener('click', getResult);
+calculatorAc.addEventListener('click', clearAll);
+
+function listNumbers(event) {
+  console.log("clicked");
+  console.log(event.target.innerHTML);
+  if (this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+  if (this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '0.';
+  var currentNUmber = event.target.innerHTML;
+  currentNumber.innerHTML += currentNUmber;
+}
+
+function operatorFunction(event) {
+  previousNum = currentNumber.innerHTML;
+  sign = event.target.innerHTML;
+  console.log(sign);
+
+  if (currentNumber.innerHTML === '' && sign === '-') {
+    currentNumber.innerHTML = '-';
+    return;
+  } else if (currentNumber.innerHTML === '') {
+    return;
+  }
+
+  if (sign !== '') {
+    getResult();
+  }
+
+  currentNumber.innerHTML = '';
+}
+
+function getResult() {
+  if (previousNum === '' || currentNumber.innerHTML === '') return;
+  var a = Number(currentNumber.innerHTML);
+  var b = Number(previousNum);
+
+  switch (sign) {
+    case '+':
+      result = a + b;
+      break;
+
+    case '-':
+      result = b - a;
+      break;
+
+    case '*':
+      result = a * b;
+      break;
+
+    case '/':
+      result = b / a;
+      break;
+  }
+
+  currentNumber.innerHTML = result;
+}
+
+function clearAll() {
+  result = '';
+  currentNumber.innerHTML = '';
+  previousNum = '';
+  sign = '';
+}
--- src/script.js
+++ src/script.js
@@ -1,35 +1,102 @@
-let numbers = document.querySelectorAll('.calculator__number');
-let operators = document.querySelectorAll('.calculator__operator');
-let ac = document.querySelector('.calculator__AC');
-let output = document.querySelector('.calculator__output');
-let previousResult = document.querySelector('.calculator__currentperation');
-let currentResult = document.querySelector('.calculator__previousoperation');
+const currentNumber = document.querySelector('.currentNumber');
+
+const calculatorNumbers = document.querySelectorAll('.calculator__number');
+
+const calculatorOperators = document.querySelectorAll('.calculator__operator');
+
+const calculatorOutput = document.querySelector('.calculator__output');
+
+const calculatorAc = document.querySelector('.calculator__AC');
+
+
+let result = '';
+let previousNum = '';
+let sign = ''
+
+
+calculatorNumbers.forEach((button) => button.addEventListener('click', listNumbers))
+calculatorOperators.forEach((button) => button.addEventListener('click', operatorFunction))
+
+
+
+calculatorOutput.addEventListener('click', getResult);
+calculatorAc.addEventListener('click', clearAll);
 
 
 
 
-let currrentReading = '';
-let previousReading = '';
 
-const updateResult = () => {
-    currentResult.innerText = currrentReading;
-    previousResult.innerText = previousReading;
+
+
+function listNumbers(event) {
+
+    console.log("clicked")
+    console.log(event.target.innerHTML)
+
+    if (this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    if (this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '0.'
+
+
+    let currentNUmber = event.target.innerHTML;
+    currentNumber.innerHTML += currentNUmber;
 }
 
 
 
-const listingNumbers = (number) => {
-    currrentReading = currrentResult.toString() + number.toString();
+
+function operatorFunction(event) {
+
+
+    previousNum = currentNumber.innerHTML;
+    sign = event.target.innerHTML;
+    console.log(sign)
+
+    if (currentNumber.innerHTML === '' && sign === '-') {
+        currentNumber.innerHTML = '-';
+        return;
+    } else if (currentNumber.innerHTML === '') {
+        return;
+    }
+
+    if (sign !== '') {
+        getResult();
+    }
+    currentNumber.innerHTML = '';
+}
+
+
+
+
+
+function getResult() {
+    if (previousNum === '' || currentNumber.innerHTML === '') return;
+
+    let a = Number(currentNumber.innerHTML);
+    let b = Number(previousNum);
+
+    switch (sign) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = b - a;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = b / a;
+            break;
+
+    }
+    currentNumber.innerHTML = result;
 
 }
-console.log(numbers.innerText)
 
+function clearAll() {
+    result = '';
+    currentNumber.innerHTML = '';
+    previousNum = '';
+    sign = '';
 
-numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        console.log((number.innerText))
-
-        listingNumbers(number.innerText)
-        updateResult()
-    })
-})
+}
